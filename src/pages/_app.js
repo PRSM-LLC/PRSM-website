378c10a2310b26d2e56c6247c9d5d7fcb2a598bc
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2023-10-14 15:32:06 -0700

    we ion needa theme ong fr

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,9 @@
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "@/styles/theme";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
   );
