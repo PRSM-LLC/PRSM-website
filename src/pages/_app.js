934da728d77dd17872ed0f0991f96f8e97e859ca
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2023-10-14 18:44:06 -0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Analytics />
       <Component {...pageProps} />
     </ChakraProvider>
   );
